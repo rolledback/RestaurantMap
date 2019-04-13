d72065def75fb729d093b9b8ba4f69216d362ca4
rolledback <matthew.rayermann@gmail.com>
2019-04-13 10:45:04 -0700

    Use https for images

--- RestaurantMapWebsite/ts/MapIcons.ts
+++ RestaurantMapWebsite/ts/MapIcons.ts
@@ -1,9 +1,9 @@
 // https://groups.google.com/forum/#!msg/Google-Maps-API/2k3T5hwI5Ck/RRso0D2jB1oJ
 export default {
-    blue_dot: "http://maps.google.com/mapfiles/ms/micons/blue-dot.png",
-    green_dot: "http://maps.google.com/mapfiles/ms/micons/green-dot.png",
-    orange_dot: "http://maps.google.com/mapfiles/ms/micons/orange-dot.png",
-    yellow_dot: "http://maps.google.com/mapfiles/ms/micons/yellow-dot.png",
-    red_dot: "http://maps.google.com/mapfiles/ms/micons/red-dot.png",
-    purple_dot: "http://maps.google.com/mapfiles/ms/micons/purple-dot.png",
+    blue_dot: "https://maps.google.com/mapfiles/ms/micons/blue-dot.png",
+    green_dot: "https://maps.google.com/mapfiles/ms/micons/green-dot.png",
+    orange_dot: "https://maps.google.com/mapfiles/ms/micons/orange-dot.png",
+    yellow_dot: "https://maps.google.com/mapfiles/ms/micons/yellow-dot.png",
+    red_dot: "https://maps.google.com/mapfiles/ms/micons/red-dot.png",
+    purple_dot: "https://maps.google.com/mapfiles/ms/micons/purple-dot.png",
 };
